1cc0e6366d727fbd5482921702b03509e9d842cd
Dima Mazur <rated_demon@ukr.net>
2018-01-29 01:20:44 +0200

    commit for heroku

--- new_backend/src/db.js
+++ new_backend/src/db.js
@@ -1,11 +1,19 @@
 import Sequelize from 'sequelize';
+import env from 'dotenv';
 
-const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+env.config();
+
+const dbString = process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const ssl = !!process.env.DATABASE_URL;
+const db = new Sequelize(dbString, {
+    dialectOptions: {ssl}
+});
+/*const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT
 });
-
+*/
 // db.authenticate().then(function(errors) { console.log(errors) });
 
 db.sync()
