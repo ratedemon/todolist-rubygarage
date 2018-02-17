17cb521968f942940e1a3c58b0a8a5c6bd6a4822
Dima Mazur <rated_demon@ukr.net>
2018-02-17 20:42:28 +0200

    fix animation

--- frontend/src/app/shared/animations.ts
+++ frontend/src/app/shared/animations.ts
@@ -1,37 +1,23 @@
-import { trigger,state,style,transition,animate,keyframes, query, stagger } from '@angular/animations';
-
-// export const myAnim = trigger('myAnim', [
-//       transition('* => *', [
-
-//         query(':enter', style({ opacity: 0 }), {optional: true}),
-
-//         query(':enter', stagger('300ms', [
-//           animate('1s ease-in', keyframes([
-//             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-//             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-//             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-//           ]))]), {optional: true})
-//       ])
-//     ])
+import {trigger, state, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 export const myAnim = trigger('myAnim', [
     state('*', style({transform: 'translateX(0)', opacity: 1})),
     transition(':enter', [
-      style({transform: 'translateX(-100%)', opacity: 0}),
-      animate(500)
+        style({transform: 'translateX(-50%)', opacity: 0}),
+        animate(500)
     ]),
     transition(':leave', [
-      animate(500, style({transform: 'translateX(100%)', opacity: 0}))
+        animate(500, style({transform: 'translateX(50%)', opacity: 0}))
     ])
-  ]);
+]);
 
-export const formAnim = trigger('formAnim',[
-  state('*', style({transform: 'translateX(0)', opacity: 1})),
+export const formAnim = trigger('formAnim', [
+    state('*', style({transform: 'translateX(0)', opacity: 1})),
     transition(':enter', [
-      style({transform: 'translateY(20%)', opacity: 0}),
-      animate(300)
+        style({transform: 'translateY(20%)', opacity: 0}),
+        animate(300)
     ]),
     transition(':leave', [
-      animate(300, style({transform: 'translateY(20%)', opacity: 0}))
+        animate(300, style({transform: 'translateY(20%)', opacity: 0}))
     ])
 ]);
