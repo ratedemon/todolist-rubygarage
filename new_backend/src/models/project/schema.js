7bdc0e9379b29310398baeef1c47c2d71d6a44d7
Dima Mazur <rated_demon@ukr.net>
2018-02-17 20:19:36 +0200

    update project validation

--- new_backend/src/models/project/schema.js
+++ new_backend/src/models/project/schema.js
@@ -1,27 +1,24 @@
 import Joi from 'joi';
 
-const schemaCreate = Joi.object().keys({
-    name: Joi.string().min(1).max(256).required(),
+const schemeDefault = {
     email: Joi.string().required().regex(/[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/, "email"),
     id: Joi.number().integer().required()
-});
+};
 
-const schemeUpdate = Joi.object().keys({
-    name: Joi.string().min(1).max(256).required(),
-    email: Joi.string().required().regex(/[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/, "email"),
-    project_id: Joi.number().integer().required(),
-    id: Joi.number().integer().required()
-});
+const schemaCreate = Joi.object().keys(
+    Object.assign({}, schemeDefault,{name: Joi.string().min(1).max(256).required()})
+);
 
-const schemeDelete = Joi.object().keys({
-    email: Joi.string().required().regex(/[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/, "email"),
-    user_id: Joi.number().integer().required(),
-    id: Joi.number().integer().required()
-});
+const schemeUpdate = Joi.object().keys(
+    Object.assign({}, schemeDefault, {name: Joi.string().min(1).max(256).required(),project_id: Joi.number().integer().required()})
+);
 
-const schemeGet = Joi.object().keys({
-    email: Joi.string().required().regex(/[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}/, "email"),
-    id: Joi.number().integer().required()
-});
+const schemeDelete = Joi.object().keys(
+    Object.assign({}, schemeDefault, {user_id: Joi.number().integer().required()})
+);
+
+const schemeGet = Joi.object().keys(
+    schemeDefault
+);
 
 export {schemaCreate, schemeUpdate, schemeDelete, schemeGet};
